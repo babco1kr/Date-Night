32daeaf23ae9cd2146f48821f46aed41de06c586
Kyle Babcock <kylerbabcock@gmail.com>
2019-03-14 12:24:43 -0400

    basic api calls added

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,1 +1,32 @@
 // Global Variables
+$(document).ready(function() {
+
+
+// Function for calling movie API
+function movies () {
+var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=d9dbc09a5b4424d83367d7f502248bf6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+
+
+$.ajax({
+    url :queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response);
+})
+}
+movies();
+
+// Function for calling food API
+function food () {
+    var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+winterpark+FL&key=AIzaSyBLMlKcGTafBPYMN1Ybe9oe4JVWHYFLFIE";
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+})
+}
+food();
+})
