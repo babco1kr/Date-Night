e193aabbb64fda4a88cda9f002712d5157025d99
Jean Valderrama <jeanvalderrama@Jeans-MacBook-Pro.local>
2019-03-14 14:56:42 -0400

    more html

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
 
 
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+      });
+            
+
 // Function for calling movie API
 function movies () {
 var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=d9dbc09a5b4424d83367d7f502248bf6&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
